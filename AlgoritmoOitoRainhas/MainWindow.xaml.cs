2301de0ba6b6429f881a1ecf0497c0c20027b84b
Natan Martins <natan.n.martins20@gmail.com>
2023-06-13 21:12:58 -0300

    Adicionar arquivos de projeto.

---
+++ AlgoritmoOitoRainhas/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using AlgoritmoOitoRainhas.src;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AlgoritmoOitoRainhas
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+                   
+        public PlaceQueen placeQueen;
+        public MainWindow()
+        {
+            InitializeComponent();
+            placeQueen = new PlaceQueen();
+        }
+
+        private void btnSolve_Click(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(txtBoardSize.Text, out placeQueen.boardSize))
+            {
+                if (placeQueen.boardSize >= 4)
+                {
+                    placeQueen.SolveNQueens();
+                    DrawBoard();
+                }
+                else
+                {
+                    MessageBox.Show("O tamanho do tabuleiro deve ser maior ou igual a 4.", "Erro", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Digite um número válido para o tamanho do tabuleiro.", "Erro", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+        
+
+        private void DrawBoard()
+        {
+            gridBoard.Children.Clear();
+            gridBoard.RowDefinitions.Clear();
+            gridBoard.ColumnDefinitions.Clear();
+
+            for (int i = 0; i < placeQueen.boardSize; i++)
+            {
+                gridBoard.RowDefinitions.Add(new RowDefinition());
+                gridBoard.ColumnDefinitions.Add(new ColumnDefinition());
+            }
+
+            for (int row = 0; row < placeQueen.boardSize; row++)
+            {
+                for (int col = 0; col < placeQueen.boardSize; col++)
+                {
+                    var border = new Border();
+                    border.SetValue(Grid.RowProperty, row);
+                    border.SetValue(Grid.ColumnProperty, col);
+
+                    var textBlock = new TextBlock();
+                    textBlock.Text = (placeQueen.board[row, col] == 1) ? "♛" : string.Empty;
+                    textBlock.FontSize = 24;
+
+                    border.Child = textBlock;
+                    gridBoard.Children.Add(border);
+                }
+            }
+
+            gridBoard.Visibility = Visibility.Visible;
+        }
+    }
+}
---
+++ AlgoritmoOitoRainhas/src/PlaceQueen.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoritmoOitoRainhas.src
+{
+    public class PlaceQueen
+    {
+        public int[,] board;
+        public int boardSize;
+        public void SolveNQueens()
+        {
+            board = new int[boardSize, boardSize];
+            PlaceQueens(0);
+        }
+
+        private bool PlaceQueens(int col)
+        {
+            if (col >= boardSize)
+            {
+                return true; // Todas as rainhas foram colocadas com segurança
+            }
+
+            for (int row = 0; row < boardSize; row++)
+            {
+                if (IsSafe(row, col))
+                {
+                    board[row, col] = 1; // Coloca a rainha no tabuleiro
+
+                    if (PlaceQueens(col + 1))
+                    {
+                        return true;
+                    }
+
+                    board[row, col] = 0; // Remove a rainha do tabuleiro (backtracking)
+                }
+            }
+
+            return false; // Nenhuma posição segura encontrada nesta coluna
+        }
+
+        private bool IsSafe(int row, int col)
+        {
+            // Verifica se não há rainhas na mesma linha
+            for (int i = 0; i < col; i++)
+            {
+                if (board[row, i] == 1)
+                {
+                    return false;
+                }
+            }
+
+            // Verifica a diagonal superior esquerda
+            for (int i = row, j = col; i >= 0 && j >= 0; i--, j--)
+            {
+                if (board[i, j] == 1)
+                {
+                    return false;
+                }
+            }
+
+            // Verifica a diagonal inferior esquerda
+            for (int i = row, j = col; i < boardSize && j >= 0; i++, j--)
+            {
+                if (board[i, j] == 1)
+                {
+                    return false;
+                }
+            }
+
+            return true; // Posição segura para colocar a rainha
+        }
+    }
+}
